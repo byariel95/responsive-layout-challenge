da3caaafa307ffe9d5a8bf7b5521e69d0be68b1a
byariel95 <bmonterrosoalvarado@gmail.com>
2023-11-21 18:58:37 -0600

    Day 01 Challenge

--- src/components/Day01/day-01.jsx
+++ src/components/Day01/day-01.jsx
@@ -3,9 +3,14 @@ import './style.css'
 
 export const Day01 = () => {
   return (
-    <div className='parent'>
-      <div className='child'>
-      </div>
+    <div className="container">
+    <div className="intro-content">
+        <h1>Lorem ipsum dolor sit.</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit</p>
+        <p>Iste ipsa enim delectus porro, ullam repellendus maiores</p>
+        <p>Accusantium minima iusto nobis fuga hic explicabo unde illum</p>
+        <p>Expedita cupiditate iure odit, delectus placeat optio magnam.</p>
     </div>
+</div>
   )
 }
