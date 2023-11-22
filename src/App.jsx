93f868cfebde7c816124016e530554e91f7bb361
byariel95 <bmonterrosoalvarado@gmail.com>
2023-11-21 18:27:24 -0600

    Day 01

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Day01} from './components/Day01/day-01'
+
+
+function App() {
+
+  return (
+    <>
+      <Day01/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Day01/day-01.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css'
+
+export const Day01 = () => {
+  return (
+    <div className='parent'>
+      <div className='child'>
+      </div>
+    </div>
+  )
+}
